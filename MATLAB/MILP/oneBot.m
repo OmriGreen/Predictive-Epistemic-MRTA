3ba7f0186ad97d2e8aadb6d2ac00ce03c294142e
Omri Green <40575742+OmriGreen@users.noreply.github.com>
2024-03-14 21:08:23 -0400

    stuff

--- MATLAB/MILP/oneBot.m
+++ MATLAB/MILP/oneBot.m
@@ -11,15 +11,15 @@ clear, clc
 S = 50; % The max X and max Y positions
 startLoc = [0 0]; % Start Location [x y]
 endLoc = [0 0]; % End Location [x y]
-T = 1; % Number of Tasks
+T = 4; % Number of Tasks
 
 % Cost Vector
 cV = costVector(T, startLoc, endLoc, S);
 f = transpose(cV);
 
 % Linear Equality Constraint Equation
-Aeq = calcA(T);
-beq = calcB(T);
+Aeq = calcAeq(T);
+beq = calcBeq(T);
 
 % Integer Variables
 intcon = 1:length(cV);
@@ -32,13 +32,13 @@ ub = ones(length(cV), 1) * inf;
 ub((T+1)^2+1) = T+1; % Adjust upper bound for the last variable
 
 % Inequality Constraint Equation
-A = [];
-b = [];
+A = calcIeq(T);
+b = calcBIeq(T);
 
 % Adjust dimensions of f to match Aeq
 f = [f; zeros(size(Aeq, 1) - length(f), 1)];
 
-% Solve MILP with updated options
+%Solve MILP with updated options
 rawX = intlinprog(f, intcon, A, b, Aeq, beq, lb, ub);
 x = [];
 tX = [];
@@ -51,133 +51,248 @@ for i=1:length(rawX)
 end
 x
 
+%% ======= Linear Inequality Constraints ======
+%Calculate Equality Constraints
+function bI=calcBIeq(T)
+    bI = [];
+
+    %for all n!=E y_n>=1
+    for i=1:T+1
+        bI=[bI;-1];
+    end
+    
+    %for all n!=S y_n<=T+1
+    for i=1:T+1
+        bI=[bI;(T)];
+    end
+end
+
+%Calculate the Inequality Matrix
+function I=calcIeq(T)
+    I = [];
+
+    %for all n!=E y_n>=1
+    I1 = Ieq1(T);
+    
+    %for all n!=E y_n<=T+1
+    I2 = Ieq2(T);
+
+    I = [I1; I2];
+end
+
+%for all n!=E y_n<=T+1
+function I2 = Ieq2(T)
+    I2 = [];
+    for z = 2:T+2
+        tA = [];
+        for i=1:T+2
+            for j=1:T+2
+                tA=[tA 0];
+            end
+        end
+        
+        for i=1:T+2
+            if i==z
+                tA=[tA 1];
+            else
+                tA=[tA 0];
+            end
+        end
+        I2 = [I2; tA];
+    end
+end
+
+%for all n!=E y_n>=1
+function I1 = Ieq1(T)
+    I1 = [];
+    for z = 1:T+1
+        tA = [];
+        for i=1:T+2
+            for j=1:T+2
+                tA=[tA 0];
+            end
+        end
+        
+        for i=1:T+2
+            if i==z
+                tA=[tA -1];
+            else
+                tA=[tA 0];
+            end
+        end
+        I1 = [I1; tA];
+    end
+end
 
 
-%TEMPORARY TESTING
-% for z=2:T+1
-%     rawA6 = req6(T,z)
-%     A6 = [];
-%     t6 = [];
-%     for i=1:length(rawA6)
-%         t6 = [t6 rawA6(i)];
-%         if rem(length(t6),T+2)==0
-%             A6 = [A6; t6];
-%             t6 = [];
-%         end
-%     end
-%     A6
-% end
-
-
-% ------------Linear Equality Constraints-------------
-%Creates all necessary inequality constraints
-function b = calcB(T)
-    b = [];
+%% ------------Linear Equality Constraints-------------
+%Creates all necessary equality constraints
+function beq = calcBeq(T)
+    beq = [];
 
     % A1: For all i z_iS=0
-    b = [b;0];
+    beq = [beq;0];
 
     % A2: for all j z_Ej = 0
-    b = [b;0];
+    beq = [beq;0];
 
     % A3: for all j sum_i!=E(z_ij)=1
     for i=1:T+1
-        b = [b;1];
+        beq = [beq;1];
     end
 
     % A4: for all i sum_j!=S(z_ij)=1
     for i=1:T+1
-       b = [b;-1];
+        beq = [beq;1];
     end
 
-    % A5: z_SE=0
-    %b = [b;0];
-
-    % A6: for all i~=S sum_j~=E(z_ij)-sum_j~=E(z_ji)=0 Eq(4)
-    for i=1:T
-       %b = [b;0];
+    % A5: y_S=T+1, y_E = 0
+    beq=[beq;T+1];
+    beq=[beq;0];
+    %sum_n(y_n)=T+1 + T + ... + 1
+    sum = 0;
+    for i=1:T+1
+        sum = sum+i;
     end
     
+    beq =[beq;sum];
+    
+    %A6: z_ES=0
+    beq =[beq;0];
+
+    %A7: for all i!=E z_ii-0
+    beq = [beq;0];
+
+    %A8: for all j sum_i!=S(z_ij)-sum_i!=S(z_ji)=0
+
 end
 
-% Creates the inequality Matrix
-function A = calcA(T)
+% Creates the Equality Matrix
+function Aeq = calcAeq(T)
     % Calculates all rows of A for the given number of Tasks
-    % For all i z_iS=0
-    A1 = req1(T);
-
-    % for all j z_Ej = 0
-    A2 = req2(T);
-
-    % for all j sum_i!=E(z_ij)=1
-    A3 = req3(T);
-        
-    % for all i sum_j!=S(z_ij)=1
-    A4 = req4(T);
-
-    %z_SE=0
-    A5 = req5(T); 
-
-    % A6: for all i~=S sum_j~=E(z_ij)-sum_j~=E(z_ji)=0 Eq(4)
-    d = req6(T) %Causing conflict
-    A6 = [];
-    A = [A1;A2;A3;A4; A5; A6];
+    A1 = req1(T); % For all i z_iS=0
+    A2 = req2(T); % for all j z_Ej = 0
+    A3 = req3(T); % for all j sum_i!=E(z_ij)=1
+    A4 = req4(T); % for all i sum_j!=S(z_ij)=1
+    A5 = req5(T); % for all ij z_ij -> y_i = y_j+1 (y_S=T+1, y_E = 0)
+    A6 = req6(T); %A6: z_ES=0
+    A7 = req7(T); %A7: for all i!=E z_ii=0
+    A8 = req8(T) %A8: for all j sum_i!=S(z_ij)-sum_i!=S(z_ji)=0
+    Aeq = [A1;A2;A3;A4;A5;A6;A7];
 end
 
+%A8: for all j sum_i!=S(z_ij)-sum_i!=S(z_ji)=0
+function A8 = req8(T)
+    A8 = [];
 
-% A6: for all i~=S j~=E z_ij-z_ji=0 Eq(4)
-function A6 = req6(T)
-    A6 = [];
-    %Goes through all potential j which are not in End
-    for jC=1:T+1
-        %Goes through all potential i which are not in Start
-        for iC = 2:T+2
-            tA = []; %Temporary array
-            %Goes Through all potential j values
-            for j=1:T+2
-                %Goes Through all possible i values
-                for i = 1:T+2
-                    if i==iC && j==jC
-                        %jumping to
-                        tA=[tA 1];
-                    else 
-                        if j==iC
-                            tA = [tA -1];
-                        else
-                            tA = [tA 0];
-                        end
-                     end
+    for jC = 1:T+1
+        tA = [];
+        for j=1:T+2
+            for i=1:T+2
+                if jC == j && i~= 1 && i~= T+2
+                    tA = [tA 1];
+                else
+                    if jC == i && j~= 1 && j~= T+2
+                        tA = [tA -1];
+                    else
+                        tA = [tA 0];
+                    end
                 end
             end
-            %Adds last 0s to constraint
-            for z=1:T+2
-                tA = [tA 0];
-            end 
-            A6 = [A6; tA];
         end
+        for z=1:T+2
+            tA = [tA 0];
+        end
+        A8 = [A8; tA];
     end
 end
 
-%z_SE = 0
+%A7: for all i!=E z_ii-0
+function A7=req7(T)
+    A7 = [];
+     for j=1:T+2
+        for i=1:T+2
+            if i==j && j~=T+2 
+                A7=[A7 1];
+            else
+                A7=[A7 0];
+            end
+            
+        end
+    end
+    for i=1:T+2
+        A7=[A7 0];
+    end 
+end
+   
+
+
+%A6: z_ES = 0
+function A6 = req6(T)
+    A6 = [];
+
+    for i=1:T+2
+        for j=1:T+2
+            if j==T+2 && i==1
+                A6=[A6 1];
+            else
+                A6=[A6 0];
+            end
+        end
+    end
+    
+    for i=1:T+2
+        A6=[A6 0];
+    end 
+end
+
+% for all ij z_ij -> y_i = y_j+1 (y_S=T+1, y_E = 0, sum_n(y_n)=T+1)
 function A5 = req5(T)
     A5 = [];
     tA = [];
+
     for i=1:T+2
         for j=1:T+2
-            if i == 1 && j==T+2
-                tA = [tA 1];
-            else
-                tA = [tA 0];
-            end
+            tA = [tA 0];
         end
     end
-    for z=1:T+2
-        tA = [tA 0];
-    end 
-    A5 = [A5;tA];
-end
+    for i=1:T+2
+        if i == 1
+            tA = [tA 1];
+        end
+        if i ~= 1
+            tA = [tA 0];
+        end
+    end
+    A5 = [A5; tA];
     
+    tA = [];
+    for i=1:T+2
+        for j=1:T+2
+            tA = [tA 0];
+        end
+    end
+    for i=1:T+2
+        if i == T+2
+            tA = [tA 1];
+        end
+        if i ~= T+2
+            tA = [tA 0];
+        end
+    end
+    A5 = [A5; tA];
 
+    tA = [];
+    for i=1:T+2
+        for j=1:T+2
+            tA = [tA 0];
+        end
+    end
+    for i=1:T+2
+        tA = [tA 1];
+    end
+    A5 = [A5; tA];
+end
 
 % for all i sum_j!=S(z_ij)=1
 function A4 = req4(T)
@@ -190,7 +305,8 @@ function A4 = req4(T)
                 for k=1:T+2
                     if k==i
                         tA = [tA 1];
-                    else
+                    end
+                    if k~=i
                         tA = [tA 0];
                     end
                 end
@@ -211,12 +327,12 @@ function A3 = req3(T)
         tA = [];
         for j=1:T+2
             for k=1:T+2
-                if j==i && k~=1
+                if j==i
                     tA = [tA 1];
-                else
+                end
+                if j~=i
                     tA = [tA 0];
                 end
-                
             end
         end
         for z=1:T+2
