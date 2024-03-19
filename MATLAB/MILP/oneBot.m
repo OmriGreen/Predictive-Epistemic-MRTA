ee77bb46f4e7467a5df87834c4a15d064606437f
OmriGreen <omrigreen@hotmail.com>
2024-03-19 13:01:33 -0400

    All Equations up to 11 work, currently working on Equation 12

--- MATLAB/MILP/oneBot.m
+++ MATLAB/MILP/oneBot.m
@@ -12,10 +12,10 @@ clear, clc
 S = 50; % The max X and max Y positions
 startLoc = [0 0]; % Start Location [x y]
 endLoc = [0 0]; % End Location [x y]
-T = 1; % Number of Tasks
+T = 5; % Number of Tasks
 
 % Cost Vector
-cV = costVector(T, startLoc, endLoc, S);
+cV = costVector(T, S);
 f = transpose(cV);
 
 % Linear Equality Constraint Equation
@@ -30,7 +30,7 @@ lb = zeros(length(cV),1);
 
 % Upper Bound value
 ub = ones(length(cV), 1) * inf;
-ub((T+1)^2+1) = T+1; % Adjust upper bound for the last variable
+ub((T-1)^2+1) = T-1; % Adjust upper bound for the last variable
 
 % Inequality Constraint Equation
 A = calcIeq(T);
@@ -45,13 +45,14 @@ x = [];
 tX = [];
 for i=1:length(rawX)
     tX = [tX rawX(i)];
-    if rem(length(tX),T+2)==0
+    if rem(length(tX),T)==0
         x = [x; tX];
         tX = [];
     end
 end
 x
 
+A5 = req5(T)
 
 
 %% ======= Linear Inequality Constraints ======
@@ -80,8 +81,8 @@ end
 %I1: sum_j(x_Sj)<=1
 function I1=getI1(T)
     I1 = [];
-    for j = 1:T+2
-        for i = 1:T+2
+    for j = 1:T
+        for i = 1:T
             if i==1
                 I1 = [I1 1];
             else
@@ -89,14 +90,14 @@ function I1=getI1(T)
             end
         end
     end
-    I1=[I1 zeros(1,(T+2)^2)];
+    I1=[I1 zeros(1,(T)^2)];
 end
 
 %I1: sum_i(x_iS)<=1
 function I2=getI2(T)
     I2 = [];
-    for j = 1:T+2
-        for i = 1:T+2
+    for j = 1:T
+        for i = 1:T
             if j==1
                 I2 = [I2 1];
             else
@@ -104,7 +105,7 @@ function I2=getI2(T)
             end
         end
     end
-    I2 = [I2 zeros(1,(T+2)^2)];
+    I2 = [I2 zeros(1,(T)^2)];
 end
 
 
@@ -117,28 +118,28 @@ function beq = calcBeq(T)
     beq = [];
     %%============== z constraints====================================
     % A1: for all j sum_i(z_ij)=1
-    for i=1:T+2
+    for i=1:T
         beq = [beq;1];
     end
 
     % A2: for all i sum_jS(z_ij)=1
-    for i=1:T+2
+    for i=1:T
         beq = [beq;1];
     end
 
     %A3: for all j sum_i
-    for i=1:T+2
+    for i=1:T
         beq = [beq;0];
     end
 
-    %A4: for all i sum_j~=i(y_ji-y_ij)=sum_j~=i(z_ij)->sum_j~=i(y_ji-y_ij)-sum_j~=i(x_ij)=0
-    for i=2:T+2
+    %A4: Flow Through Starting Node
+    beq = [beq; 0];
+
+    %A5: Energy Updated after visting each node
+    for i=1:T
         beq = [beq;0];
     end
-    %A5: for all i sum_j~=i(y_ji-y_ij)=sum_j~=i(x_ij)->sum_j~=i(y_ji-y_ij)-sum_j~=i(x_ij)=0
-    for i=1:T+2
-        beq = [beq;0];
-    end
+
 end
 
 % Creates the Equality Matrix
@@ -150,37 +151,38 @@ function Aeq = calcAeq(T)
     A3 = req3(T); % for all j sum_i(z_ij-z_ji)=0
 
     %% Flow Constraints
-    A4 = req4(T) %sum_i(y_Si-y_iS)=sum_i,j(x_ij)-> %sum_i(y_Si-y_iS)-sum_i,j(z_ij) = 0
-    A5 = req5(T); %for all i sum_j~=i(y_ji-y_ij)=sum_j~=i(z_ij)->sum_j~=i(y_ji-y_ij)-sum_j~=i(x_ij)=0
+    %A4: Flow Through Starting Node
+    A4 = req4(T);
+
+    %A5: Energy Updated after visiting each node
+    A5 = req5(T);
     Aeq = [A1;A2;A3;A4;A5];
 end
-
-%for all i sum_j~=i(y_ji-y_ij)=sum_j~=i(x_ij)->for all i sum_j~=i(y_ji-y_ij)-sum_j~=i(x_ij)=0
-function A5=req5(T)
+    
+%A5: Energy Updated after visiting each node
+function A5 = req5(T)
     A5 = [];
-
-    for iC = 1:T+2
-        %Goes Through z
+    for iC=1:T
         tA = [];
-        for j = 1:T+2
-            for i = 1:T+2
-                %-sum_j(x_ij)
-                if i == iC && i~=j
+        %Goes over z
+        for j = 1:T
+            for i = 1:T
+                if i == iC
                     tA = [tA -1];
                 else
                     tA = [tA 0];
                 end
+                
             end
         end
 
         %Goes Through y
-        for j = 1:T+2
-            for i = 1:T+2
-                %sum_j~=i(y_ji-y_ij)
-                if j == iC
+        for j = 1:T
+            for i = 1:T
+                if j == iC && i ~= j
                     tA = [tA 1];
                 else
-                    if i == iC
+                    if i == iC && i ~= j
                         tA = [tA -1];
                     else
                         tA = [tA 0];
@@ -188,49 +190,39 @@ function A5=req5(T)
                 end
             end
         end
-        A5 = [A5; tA];
+        A5 =[A5; tA];
     end
 end
 
-%IN PROGRESS IS WHATS BREAKING EVERYTHING -- Multi-robot long-term
-%persistent coverage with fuel constrained robots Eq 11
- %sum_i(y_Si-y_iS)=sum_i,j(x_ij)-> %sum_i(y_Si-y_iS)-sum_i,j(z_ij) = 0
- function A4 = req4(T)
-    A4 = [];
-    %All z values -sum_i,j(z_ij)
-    zVals = -ones(1,(T+2)^2);
-
-    tA = [];
-    %Creates y values
-    %sum_i(y_Si-y_iS)
-    for iC = 2:T+2
-        tA = [];
-        for j = 1:T+2
-            for i = 1:T+2
-                if i == iC && j == 1 && j~=i
-                    tA = [tA 1];
+%A4: Flow Through Starting Node
+function A4 = req4(T)
+    %Gets sum_i,h(z_ij)
+    A4 = -ones(1,(T)^2);
+    
+    %Gets sum_i(y_Si-y_iS)
+    for j = 1:T
+        for i = 1:T
+            if i==1 && i~=j
+                A4 = [A4 1];
+            else
+                if j == 1 && i~=j
+                    A4 = [A4 -1];
                 else
-                    if j == iC && i == 1 && j~=i
-                        tA = [tA -1];
-                    else
-                        tA = [tA 0];
-                    end
+                    A4 = [A4 0];
                 end
             end
         end
-        A4 = [A4; zVals tA];
     end
-    
+end
 
- end
 
 % for all j sum_i(x_ij-x_ji)=0
 function A3 = req3(T)
     A3 = [];
-    for jC=1:T+2
+    for jC=1:T
         tA = [];
-        for j = 1:T+2
-            for i = 1:T+2
+        for j = 1:T
+            for i = 1:T
                 if j == jC
                     tA = [tA 1];
                 else
@@ -242,7 +234,7 @@ function A3 = req3(T)
                 end
             end
         end
-        tA = [tA zeros(1,(T+2)^2)];
+        tA = [tA zeros(1,(T)^2)];
         A3 = [A3; tA];
     end
 end
@@ -251,10 +243,10 @@ end
 function A2 = req2(T)
     A2 = [];
 
-    for i=1:T+2
+    for i=1:T
             tA = [];
-            for j=1:T+2
-                for k=1:T+2
+            for j=1:T
+                for k=1:T
                     if k==i
                         tA = [tA 1];
                     end
@@ -263,7 +255,7 @@ function A2 = req2(T)
                     end
                 end
             end
-            tA = [tA zeros(1,(T+2)^2)]; 
+            tA = [tA zeros(1,(T)^2)]; 
             A2 = [A2;tA];
     end
 end
@@ -272,10 +264,10 @@ end
 function A1 = req1(T)
     A1 = [];
 
-    for i=1:T+2
+    for i=1:T
         tA = [];
-        for j=1:T+2
-            for k=1:T+2
+        for j=1:T
+            for k=1:T
                 if j==i
                     tA = [tA 1];
                 end
@@ -284,7 +276,7 @@ function A1 = req1(T)
                 end
             end
         end
-        tA = [tA zeros(1,(T+2)^2)];
+        tA = [tA zeros(1,(T)^2)];
         A1 = [A1;tA];
     end
 end
@@ -292,12 +284,12 @@ end
 %% ---------Cost Vector Functions----------
 
 %Creates a cost Vector for a MILP problem
-function cV = costVector(T,startLoc,endLoc, S)
+function cV = costVector(T, S)
     cV = [];
-    locations = setup(T, startLoc, endLoc, S);
+    locations = setup(T, S);
     
-    for i=1:T+2
-        for j=1:T+2
+    for i=1:T
+        for j=1:T
             x1=locations(i,1);
             x2=locations(j,1);
             y1=locations(i,2);
@@ -305,7 +297,7 @@ function cV = costVector(T,startLoc,endLoc, S)
             cV=[cV findDist(x1,x2,y1,y2)];
         end
     end
-    cV = [cV zeros(1,(T+2)^2)];
+    cV = [cV zeros(1,(T)^2)];
 end
 
 %Finds the distance between two points
@@ -314,14 +306,12 @@ function distance = findDist(x1,x2,y1,y2)
 end
 
 %Creates Locations for all tasks with a set amount of tasks
-function taskLocations = setup(T, startLoc, endLoc, S)
+function taskLocations = setup(T, S)
     maxSize = S;
     taskLocations = [];
-    taskLocations = [taskLocations; startLoc];
     for i=1:T
         a = floor(maxSize*rand);
         b = floor(maxSize*rand);
         taskLocations = [taskLocations; [a,b]];
     end
-    taskLocations = [taskLocations;endLoc];
 end
